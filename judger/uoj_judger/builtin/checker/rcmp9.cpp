48088a1f478949f451bd189a80289e6c92b78126
krn1pnc <krn1pnc@outlook.com>
2023-08-06 18:58:24 +0800

    chore: bump testlib and builtin checkers to latest

--- judger/uoj_judger/builtin/checker/rcmp9.cpp
+++ judger/uoj_judger/builtin/checker/rcmp9.cpp
@@ -1,32 +1,28 @@
 #include "testlib.h"
-#include <cmath>
 
 using namespace std;
 
 const double EPS = 1E-9;
 
-int main(int argc, char * argv[])
-{
-    setName("compare two sequences of doubles, max absolute or relative error = %.10lf", EPS);
+int main(int argc, char *argv[]) {
+    setName("compare two sequences of doubles, max absolute or relative error = %.10f", EPS);
     registerTestlibCmd(argc, argv);
 
     int n = 0;
-    double j, p;
+    double j = 0, p = 0;
 
-    while (!ans.seekEof()) 
-    {
+    while (!ans.seekEof()) {
         n++;
         j = ans.readDouble();
         p = ouf.readDouble();
-        if (!doubleCompare(j, p, EPS))
-        {
-            quitf(_wa, "%d%s numbers differ - expected: '%.7lf', found: '%.7lf', error = '%.7lf'",
-                n, englishEnding(n).c_str(), j, p, doubleDelta(j, p));
+        if (!doubleCompare(j, p, EPS)) {
+            quitf(_wa, "%d%s numbers differ - expected: '%.10f', found: '%.10f', error = '%.10f'",
+                  n, englishEnding(n).c_str(), j, p, doubleDelta(j, p));
         }
     }
 
     if (n == 1)
-        quitf(_ok, "found '%.9lf', expected '%.9lf', error '%.9lf'", p, j, doubleDelta(j, p));
+        quitf(_ok, "found '%.10f', expected '%.10f', error '%.10f'", p, j, doubleDelta(j, p));
 
     quitf(_ok, "%d numbers", n);
 }
