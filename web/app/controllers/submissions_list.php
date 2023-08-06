21643dd9bbde7766ae66265ce4724c5470cec85d
krn1pnc <krn1pnc@outlook.com>
2023-08-06 22:02:05 +0800

    feat: better input style

--- web/app/controllers/submissions_list.php
+++ web/app/controllers/submissions_list.php
@@ -48,8 +48,8 @@
 		</div>
 		<div id="form-group-score" class="form-group">
 			<label for="input-min_score" class="control-label"><?= UOJLocale::get('score range')?>:</label>
-			<input type="text" class="form-control input-sm" name="min_score" id="input-min_score" value="<?= $q_min_score ?>" maxlength="3" style="width:4em" placeholder="0" />
-			<label for="input-max_score" class="control-label">~</label>
+			<input type="text" class="form-control input-sm" name="min_score" id="input-min_score" value="<?= $q_min_score ?>" maxlength="3" style="width:4em; margin-right: 0.25em;" placeholder="0" />
+			<label for="input-max_score" style="margin-right: 0.25em;" class="control-label">~</label>
 			<input type="text" class="form-control input-sm" name="max_score" id="input-max_score" value="<?= $q_max_score ?>" maxlength="3" style="width:4em" placeholder="100" />
 		</div>
 		<div id="form-group-language" class="form-group">
