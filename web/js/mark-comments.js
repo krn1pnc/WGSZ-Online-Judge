f1508374880c2d99095077ba071e4bc523e33927
krn1pnc <krn1pnc@outlook.com>
2023-08-07 23:30:25 +0800

    feat: disable markdown of comments of comments

--- web/js/mark-comments.js
+++ web/js/mark-comments.js
@@ -6,11 +6,6 @@ function markComments() {
         text = text.replace(/&gt;/g, '>').replace(/&lt;/g, '<').replace(/&amp;/g, '&');
         $(this)[0].innerHTML = marked.parse(text);
     });
-    $('.comtbox6').each(function () {
-        var text = $(this)[0].innerHTML;
-        text = text.replace(/&gt;/g, '>').replace(/&lt;/g, '<').replace(/&amp;/g, '&');
-        $(this)[0].innerHTML = marked.parse(text);
-    });
 }
 
 $(function () {
